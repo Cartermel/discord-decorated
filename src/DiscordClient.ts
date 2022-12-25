991f3ba0739e0266cbe280ce44f514996727e16b
Carter <c.melnychuk97@gmail.com>
2022-12-24 17:35:19 -0700

    cleanup imports

--- src/DiscordClient.ts
+++ src/DiscordClient.ts
@@ -1,6 +1,5 @@
 import { Client, Message } from "discord.js";
 import { CommandProvider } from "./CommandProvider";
-import { CommandModule } from "./models/CommandModule";
 import {
   CommandErrorCallback,
   DiscordClientOptions,
